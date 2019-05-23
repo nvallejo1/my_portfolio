f153eead947c108e3320fe4f596e7ad04b05da7f
eortiz16 <eortiz16@csub.edu>
2019-05-22 21:14:04 -0700

    initial push

---
+++ src/resumeData.js
@@ -0,0 +1,92 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Naomi Vallejo",
+    "role": "Administrative Assistant and Intake Coordinator",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like enjoy \.\..",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        }
+      ],
+    "aboutme":"I am an Administrative Assistant and Intake Coordinator at REACH, Resource for Education, Communication and Housing. Currently I am working towards growing a team of clients and staff at our recently opened satellite office in Monrovia. I was asked to transfer to Monrovia as part of the administrative and intake team. I wear different hats according to our needs. I play a supportive role as an administrative assistant, organizing and creating client files and acquiring authorizations to provide services. I’ve also taken the lead as part of our intake team by focusing on developing relationships with local nonprofit organizations, chamber members and other organizations to build our client base. So far, it has been a challenge that has allowed me to grow and apply my skills to different tasks.",
+    "address":"Whittier, California",
+    "website":"nvallejo246@gmail.com",
+    "education":[
+      {
+        "UniversityName":"University of California, San Diego",
+        "specialization":"Pharmaclogical Chemistry",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2014",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Research"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+
+  export default resumeData
